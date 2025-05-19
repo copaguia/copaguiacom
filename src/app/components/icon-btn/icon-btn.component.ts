2a9c485be997f1de552198373163fc0cc342a101
copaguia.com <copaguia.com@gmail.com>
2025-05-19 02:22:10 +0000

    casi listo para un boton reusables al 100% por tipos de boton material

--- src/app/components/icon-btn/icon-btn.component.ts
+++ src/app/components/icon-btn/icon-btn.component.ts
@@ -1,28 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 interface iconBtnInterface {
-  ariaLabel?: string;  
-  color?: 'primary' | 'accent' | 'warn' | 'default';
+  ariaLabel?: string;
+  color?: 'primary' | 'accent' | 'warn' | 'default' ;
   disabled?: boolean;
   route?: string;
   icon: string;
+  name?: string;
+  type?:  'mat-fab' | 'mat-mini-fab' | 'mat-icon-button' | 'mat-raised-button' | 'mat-flat-button' | 'mat-stroked-button'; // Agregados nuevos tipos
+}
+
+enum typeEnumV19 {  
+  'Basic' = 'mat-button',
+  'Raised' = 'mat-raised-button',
+  'Stroked' = 'mat-stroked-button',
+  'Flat' = 'mat-flat-button',
+  'Icon' = 'mat-icon-button',
+  'FAB' = 'mat-fab',
+  'Mini FAB' = 'mat-mini-fab',
+  'Extended Fab' = 'mat-fab extended'
+}
+
+enum typeEnum20 {
+  'Text' = 'matButton',
+  'Elevated' = 'matButton="elevated"',
+  'Outlined' = 'matButton="outlined"',
+  'Filled' = 'matButton="filled"',
+  'Tonal' = 'matButton="tonal"',
+  'Icon' = 'matIconButton',
+  'Floating Action Button ' = 'matFab',
+  'Mini FAB' = 'matMiniFab',
+  'Extended FAB' = 'matFab extended'
 }
 
 @Component({
   selector: 'app-icon-btn',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [ CommonModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './icon-btn.component.html',
   styleUrl: './icon-btn.component.css'
 })
 export class IconBtnComponent {
 
-  @Input() conector!: iconBtnInterface;
-
-  //conector = input<iconBtnInterface>();
+  // creo una varible signal de tipo Interface
+  conector = input<iconBtnInterface>();
 
 }
