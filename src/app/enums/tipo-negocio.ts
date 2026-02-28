419fff5edb1986fde593d0446625fdb2bb93e6e8
copaguia.com <copaguia.com@gmail.com>
2026-02-28 18:41:25 +0000

    Creacion enum tipoNegocio

---
+++ src/app/enums/tipo-negocio.ts
@@ -0,0 +1,69 @@
+export enum TipoNegocio {
+    // GASTRONOMIA
+    RESTAURANTE         = 'Restaurante',
+    COMIDA_RAPIDA       = 'Comida Rápida',
+    PANADERIA           = 'Panadería',
+    HELADERIA           = 'Heladería',
+    REPOSTERIA          = 'Repostería',
+    CAFETERIA           = 'Cafetería',
+    LICORERA            = 'Licorera',
+    CIGARRERIA          = 'Cigarrería',
+    CARNICERIA          = 'Carnicería',
+    FRUVER              = 'Fruver',
+  
+    // COMERCIO / RETAIL
+    TIENDA_ROPA         = 'Tienda de Ropa',
+    PAPELERIA           = 'Papelería',
+    SUPERMERCADO        = 'Supermercado',
+    ZAPATERIA           = 'Zapatería',
+    TIENDA_MASCOTAS     = 'Tienda de Mascotas',
+    JOYERIA             = 'Joyería',
+    ACCESORIOS_DAMA     = 'Accesorios para Dama',
+    CACHARRERIA         = 'Cacharrería',
+    MISCELANEA          = 'Miscelánea',
+    REMATE              = 'Remates',
+    DESECHABLES         = 'Desechables',
+    LENCERIA_HOGAR      = 'Lencería para el Hogar',
+  
+    // SALUD Y BELLEZA
+    DROGUERIA           = 'Droguería',
+    BARBERIA            = 'Barbería',
+    PELUQUERIA          = 'Peluquería',
+    PELUQUERIA_CANINA   = 'Peluquería Canina',
+    ESTETICA            = 'Centro de Estética',
+  
+    // TECNICO Y AUTOMOTRIZ
+    FERRETERIA          = 'Ferretería',
+    CERRAJERIA          = 'Cerrajería',
+    TALLER_MOTOS        = 'Taller de Motos',
+    TALLER_CARROS       = 'Taller de Carros',
+    MECANICO            = 'Mecánico',
+    AUTOLAVADO          = 'Autolavado',
+    HERRERIA            = 'Herrería',
+    REPUESTOS_BICIS     = 'Repuestos de Bicicletas',
+    REPUESTOS_AUTOS     = 'Repuestos de Autos',
+    REPARACIONES        = 'Reparaciones Técnicas',
+  
+    // TECNOLOGIA Y OFICINA
+    TIENDA_TELEFONOS    = 'Tienda de Teléfonos',
+    TECNOLOGIA          = 'Tecnología',
+    LITOGRAFIA          = 'Litografía',
+    LIBRERIA            = 'Librería',
+    ENCOMIENDAS         = 'Encomiendas y Envíos',
+  
+    // INSTITUCIONAL Y OTROS
+    BANCO               = 'Banco',
+    PARROQUIA           = 'Parroquia',
+    IGLESIA             = 'Iglesia',
+    INMOBILIARIA        = 'Inmobiliaria',
+    PARQUEADERO         = 'Parqueadero',
+    BILLAR              = 'Billar',
+    FABRICA             = 'Fábrica',
+    PROFESIONAL_INDEP   = 'Profesional Independiente'
+  }
+  
+  
+  
+  
+  
+  // Fin del enum detallado de tipos de negocios para Cooaguia.com
